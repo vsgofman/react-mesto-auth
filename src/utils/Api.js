f26407fe23b8c835f8aa335ea0f0da98f28ed788
Viktor Gofman <vsgof@yandex.ru>
2023-07-01 14:47:58 +0300

    feat: универсальный метод запроса с проверкой ответа

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -18,63 +18,67 @@ class Api extends React.Component {
     }
     return res.json();
   }
+  
+  _request(url, options) {
+    return fetch(url, options).then(this._getResponseData)
+  }
 
   getProfile() {
-    return fetch(`${this._url}/users/me`, {
+    return this._request(`${this._url}/users/me`, {
       headers: this._headers
-    }).then(res => this._getResponseData(res))
+    })
   }
 
   editProfile(name, about) {
-    return fetch(`${this._url}/users/me`, {
+    return this._request(`${this._url}/users/me`, {
       method: "PATCH",
       headers: this._headers,
       body: JSON.stringify({
         name,
         about
       })
-    }).then(res => this._getResponseData(res))
+    })
   }
 
   editAvatar(url) {
-    return fetch(`${this._url}/users/me/avatar`, {
+    return this._request(`${this._url}/users/me/avatar`, {
       method: "PATCH",
       headers: this._headers,
       body: JSON.stringify({
         avatar: `${url}`
       })
-    }).then(res => this._getResponseData(res))
+    })
   }
 
   getInitialCards() {
-    return fetch(`${this._url}/cards`, {
+    return this._request(`${this._url}/cards`, {
       headers: this._headers,
-    }).then(res => this._getResponseData(res))
+    })
   }
 
   addCard(name, link) {
-    return fetch(`${this._url}/cards`, {
+    return this._request(`${this._url}/cards`, {
       method: "POST",
       headers: this._headers,
       body: JSON.stringify({
         name,
         link
       })
-    }).then(res => this._getResponseData(res))
+    })
   }
 
   deleteCard(id) {
-    return fetch(`${this._url}/cards/${id}`, {
+    return this._request(`${this._url}/cards/${id}`, {
       method: "DELETE",
       headers: this._headers
-    }).then(res => this._getResponseData(res))
+    })
   }
 
   changeLikeCardStatus(id, isLiked) {
-    return fetch(`${this._url}/cards/${id}/likes`, {
+    return this._request(`${this._url}/cards/${id}/likes`, {
       method: `${isLiked ? 'PUT' : 'DELETE'}`,
       headers: this._headers
-    }).then(res => this._getResponseData(res))
+    })
   }
 }
 
